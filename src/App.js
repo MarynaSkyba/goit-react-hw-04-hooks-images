398f61f1725163aae4c06b4f4e452ffcca2c6467
Maryna <marynaskyba@gmail.com>
2021-09-19 19:16:14 +0200

    add hooks modal

--- src/App.js
+++ src/App.js
@@ -42,16 +42,9 @@ export default function App() {
   const handleFormSubmit = searchImage => {
     setPage(1);
     setImages([]);
-    // setSearchImage('');
     setSearchImage(searchImage);
   };
 
-  const reset = () => {
-    setPage(1);
-    setImages([]);
-    setSearchImage('');
-    setStatus('idle');
-  };
   const handleButtonLoadMore = () => {
     setPage(p => p + 1);
   };
@@ -79,73 +72,3 @@ export default function App() {
     </div>
   );
 }
-
-// export default class App extends Component {
-//   state = {
-//     selectedImage: null,
-//     searchImage: null,
-//     images: [],
-//     page: 1,
-//     error: null,
-//     status: 'idle',
-//     showModal: false,
-//   };
-
-//   async componentDidUpdate (prevProps, prevState) {
-// const {searchImage, page} = this.state;
-
-//         if (prevState.searchImage !== searchImage || prevState.page !== page){
-//          try{
-//           this.setState({status: 'pending'})
-//          const gallery = await fetchImages(searchImage, page)
-
-//          this.setState({status: 'resolved'})
-
-//          if(searchImage.trim() ==='' || gallery.length === 0) {
-//                       return toast.error(`нет картинки с именем  ${searchImage}`)
-//                   }
-
-//          this.setState({images: [...this.state.images, ...gallery]});
-//          window.scrollTo({
-//           top: document.documentElement.scrollHeight,
-//           behavior: 'smooth',
-//         });
-//          }
-//          catch(error){
-//           this.setState({status: 'rejected'})
-//           toast.error('что-то пошло не так')
-//          }
-//       }
-//     }
-
-//   handleFormSubmit = (searchImage) => {
-//     this.setState({searchImage: searchImage, page: 1, images: []});
-//   };
-
-//   handleButtonLoadMore = () => {
-//     this.setState(p => ({page: p.page + 1}));
-//     }
-
-//     handleSelectedImage =(imageURL) => {
-//       this.setState((prevState) => ({
-//         showModal: !prevState.showModal,
-//         selectedImage: imageURL,
-//       }))}
-
-// render(){
-//   const {images, status, showModal, selectedImage, } = this.state;
-//   const showButton = images.length > 12;
-
-//   return (
-//     <div>
-//       <SearchBar onSubmit={this.handleFormSubmit} />
-//       {status === 'pending' && <Spinner />}
-//       <ImageGallery images = {images} onSelect={this.handleSelectedImage}/>
-//       {showButton && <Button  onClick={this.handleButtonLoadMore}/>}
-//     {showModal && <Modal src={selectedImage.largeImageURL} alt={selectedImage.tags} onSelect={this.handleSelectedImage} />}
-//       <Toaster />
-//     </div>
-//   )
-// }
-
-// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,41 +1,76 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { Overlay, ModalWindow } from "./Modal.styled";
 import PropTypes from 'prop-types';
 
 
-export default class Modal extends Component {
-    componentDidMount() {
-      window.addEventListener('keydown', this.handleKeyDown);
-    }
-  
-    componentWillUnmount() {
-      window.removeEventListener('keydown', this.handleKeyDown);
-    }
-  
-    handleKeyDown = e => {
-      if (e.code === 'Escape') {
-        this.props.onSelect();
-      }
-    };
-  
-    handleBackDrop = e => {
-      if (e.currentTarget === e.target) {
-        this.props.onSelect();
-      }
-    };
-  
-    render() {
-      const { src, alt } = this.props;
-     return (
-        <Overlay onClick={this.handleBackDrop}>
-          <ModalWindow>
-            <img src={src} alt={alt} />
-          </ModalWindow>
-        </Overlay>
-    
-      );
-    }
+export default function Modal ({src, alt, onSelect})  {
+useEffect(()=>{
+  window.addEventListener('keydown', handleKeyDown);
+return () => {
+  window.removeEventListener('keydown', handleKeyDown);
+}})
+
+const handleKeyDown = e => {
+  if (e.code === 'Escape') {
+    onSelect();
   }
+};
+
+const handleBackDrop = e => {
+  if (e.currentTarget === e.target) {
+    onSelect();
+  }
+};
+
+
+return (
+  <Overlay onClick={handleBackDrop}>
+    <ModalWindow>
+      <img src={src} alt={alt} />
+    </ModalWindow>
+  </Overlay>
+
+);
+
+}
+  
+
+
+
+
+// export default class Modal extends Component {
+//     componentDidMount() {
+//       window.addEventListener('keydown', this.handleKeyDown);
+//     }
+  
+//     componentWillUnmount() {
+//       window.removeEventListener('keydown', this.handleKeyDown);
+//     }
+  
+//     handleKeyDown = e => {
+//       if (e.code === 'Escape') {
+//         this.props.onSelect();
+//       }
+//     };
+  
+//     handleBackDrop = e => {
+//       if (e.currentTarget === e.target) {
+//         this.props.onSelect();
+//       }
+//     };
+  
+//     render() {
+//       const { src, alt } = this.props;
+//      return (
+//         <Overlay onClick={this.handleBackDrop}>
+//           <ModalWindow>
+//             <img src={src} alt={alt} />
+//           </ModalWindow>
+//         </Overlay>
+    
+//       );
+//     }
+//   }
 
   Modal.propTypes = {
     src: PropTypes.string.isRequired,
